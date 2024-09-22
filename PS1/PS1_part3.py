f743eaf34a59739a2bc76119ce7e3b095d915a05
Dylan Forbes <forbesd23@bbhcsd.org>
2024-09-22 17:31:54 -0500

    Add files via upload

--- PS1/PS1_part3.py
+++ PS1/PS1_part3.py
@@ -12,12 +12,16 @@ w = 2*np.pi*f
 Vpp = .1 #Voltage peak to peak
 #Vin = Vpp * np.sin(w*t) #VIn fucntion
 Vin = Vpp * np.exp(w*t*1j)
+VinQthree = np.where(Vin<0, 0, Vin)
 #%%
 def invertOpAmp(R1,R2,Vin): #Solve inverting Op Amp
     Vout = -Vin*(R2/R1)
     return Vout
 def noninvertOpAmp(R1,R2,Vin): #solve non inverting Op Amp
     Vout = Vin*(1+R1/R2)
+    return Vout
+def openOpAmp(Vinminus,Vinplus):
+    Vout = 10**4*(Vinplus-Vinplus)
     return Vout
 def PlotOpAmp(t,Vin,Vout,Q): #ploting Vin and Vout
     plt.plot(t,Vin, label = 'Vin')
@@ -35,14 +39,17 @@ RC = 10000 + (-1j)/(w*10*(10**(-9))) # solve q5 R2 value
 #%%
 QN = [0,1,2,3,4] #question number
 OpAmpIN = [N,I,N,I,I] #selecting op amp type for each Q
-R1t = [.00000001,1000,10000,10000,1000] #selecting R1 for each Q
-R2t = [.0000001,10000,1000,10000,RC] #selectig R2 for each Q
+R1t = [0.1,1000,10000,10000,1000] #selecting R1 for each Q
+R2t = [np.inf,10000,1000,10000,RC] #selectig R2 for each Q
 #%%
-for Q in QN: #Chose method to solve each question and plot each question
+for Q in QN:#Chose method to solve each question and plot each question
+    if Q == 3:
+        Vin = VinQthree
     if OpAmpIN[Q] == True:
         Vout = invertOpAmp(R1t[Q], R2t[Q], Vin)
     if OpAmpIN[Q] == False:
         Vout = noninvertOpAmp(R1t[Q], R2t[Q], Vin)
     Q = str(Q+1)
+    Vin = Vpp * np.exp(w*t*1j)
     OpAmp = PlotOpAmp(t, Vin, Vout, Q)
     plt.show()
